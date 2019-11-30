644c155fc53a711fc6b8e769dd859eb842006b04
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-30 12:28:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,7 @@
 class Student
 
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
-
+  def initialize(student_hash)
   student_hash.each do |attribute, value|
      self.send("#{attribute}=", value)
    end
