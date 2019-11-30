58af6911b84c4e8c381bd79a364a04ef6e53cd82
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-30 12:32:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,30 +1,30 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
 
-   def initialize(student_hash)
+  @@all = []
+
+  def initialize(student_hash)
     student_hash.each do |attribute, value|
-     self.send("#{attribute}=", value)
+      self.send("#{attribute}=", value)
     end
-   end
-   @@all << self
- end
-
+    @@all << self
+  end
 
   def self.create_from_collection(students_array)
     students_array.each do |hash|
       Student.new(hash)
     end
+  end
 
-   def add_student_attributes(attributes_hash)
+  def add_student_attributes(attributes_hash)
     attributes_hash.each do |attribute, value|
-     self.send("#{attribute}=", value)
-   end
-   self
- end
+      self.send("#{attribute}=", value)
+    end
+    self
+  end
 
-   def self.all
-     @@all
-   end
+  def self.all
+    @@all
   end
 end
